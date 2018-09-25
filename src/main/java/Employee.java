a6779b6a24c44fcab13578a034bc61d6d405764b
nilesh.devdas@gmail.com <nilesh.devdas@gmail.com>
2018-09-25 11:05:08 +0530

    Initial Commit

---
+++ src/main/java/Employee.java
@@ -0,0 +1,14 @@
+
+public class Employee {
+
+    private long empid; 
+
+    public void setEmpid(long empid){
+        this.empid=empid;
+    }
+
+    public long getEmpid(){
+        return this.empid;
+    }
+
+}
